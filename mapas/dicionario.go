66716958f6258a05e2ca8f64f874a0133d835bb6
Mateus Mello <mateuspmello@gmail.com>
2019-10-30 23:43:53 -0300

    subindo os testes com maps

--- mapas/dicionario.go
+++ mapas/dicionario.go
@@ -1,24 +1,53 @@
 package mapas
 
-import "errors"
-
 //Dicionario de palavras
 type Dicionario map[string]string
 
-//ErrNaoEncontrado é a mensagem de erro não encontrado
-var ErrNaoEncontrado = errors.New("não foi possível encontrar a palavra que você procura")
+//ErrDicionario erros do dicionario
+type ErrDicionario string
+
+var (
+	errNaoEncontrado    = ErrDicionario("não foi possível encontrar a palavra que você procura")
+	errDuplicado        = ErrDicionario("palavra %s já existe. Descrição: %s")
+	errPalavraNaoExiste = ErrDicionario("palavra não existe")
+)
 
 //Busca uma palavra
 func (d Dicionario) Busca(palavra string) (string, error) {
 	definicao, existe := d[palavra]
 	if !existe {
-		return palavra, ErrNaoEncontrado
+		return palavra, errNaoEncontrado
 	}
 	return definicao, nil
 }
 
 //Adiciona uma palavra
 func (d Dicionario) Adiciona(palavra string, descricao string) (string, error) {
+	_, existe := d[palavra]
+	if existe {
+		return d[palavra], errDuplicado
+	}
 	d[palavra] = descricao
 	return descricao, nil
+}
+
+func (err ErrDicionario) Error() string {
+	return string(err)
+}
+
+//Atualiza faz a atualização da definição
+func (d Dicionario) Atualiza(palavra string, novaDefinicao string) error {
+	_, err := d.Busca(palavra)
+	switch err {
+	case errNaoEncontrado:
+		return errPalavraNaoExiste
+	case nil:
+		d[palavra] = novaDefinicao
+	}
+	return nil
+}
+
+//Deletar faz a remoção do elemento do map
+func (d Dicionario) Deletar(palavra string) {
+	delete(d, palavra)
 }
--- mapas/dicionario_test.go
+++ mapas/dicionario_test.go
@@ -2,19 +2,67 @@ package mapas
 
 import "testing"
 
-var palavraNaoExiste = "Palavra não existe %s"
-var esperavaRecebeu = "Esperava %s. Recebeu %s."
+const (
+	palavraNaoExiste  = "Palavra não existe %s"
+	palavraDuplicada  = "Tentou inserir palavra duplicada %s"
+	esperavaRecebeu   = "Esperava %s. Recebeu %s."
+	atualizaDefinicao = "Não foi atualizada a palavra %s"
+	deletarDefinicao  = "Não foi deletada a palavra %s"
+)
 
 func TestAdiciona(t *testing.T) {
 
 	t.Run("Teste de adicionar um elemento", func(t *testing.T) {
 		dicionario := Dicionario{}
+		// dicionario := Dicionario{"carro": "carro é um bem de consumo"}
 		palavra := "carro"
 		definicao := "carro é um bem de consumo"
-		dicionario.Adiciona(palavra, definicao)
+		descricao, err := dicionario.Adiciona(palavra, definicao)
+		if err != nil {
+			t.Fatalf(palavraDuplicada, palavra)
+		}
 
+		comparaErro(t, descricao, definicao)
 		comparaDefinicao(t, dicionario, palavra, definicao)
 	})
+
+}
+
+func TestAtualiza(t *testing.T) {
+	t.Run("Atualiza definicao", func(t *testing.T) {
+		dicionario := Dicionario{"carro": "carro é um bem de consumo"}
+		palavra := "carro"
+		novaDefinicao := "carro é um patrimônio"
+		err := dicionario.Atualiza(palavra, novaDefinicao)
+		if err != nil {
+			t.Errorf(palavraNaoExiste, palavra)
+		}
+
+		if dicionario[palavra] != novaDefinicao {
+			t.Fatalf(novaDefinicao, palavra)
+		}
+	})
+}
+
+func TestDeletar(t *testing.T) {
+	t.Run("Deleta definicao", func(t *testing.T) {
+		dicionario := Dicionario{"carro": "carro é um bem de consumo"}
+		palavra := "carro"
+		dicionario.Deletar(palavra)
+
+		def, _ := dicionario.Busca(palavra)
+		if def == "carro é um bem de consumo" {
+			t.Fatalf("Não deletou %s", palavra)
+		}
+
+	})
+}
+
+func comparaErro(t *testing.T, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf(esperavaRecebeu, want, got)
+	}
 }
 
 func comparaDefinicao(t *testing.T, d Dicionario, p string, de string) {
@@ -24,8 +72,5 @@ func comparaDefinicao(t *testing.T, d Dicionario, p string, de string) {
 	if err != nil {
 		t.Fatalf(palavraNaoExiste, p)
 	}
-
-	if got != want {
-		t.Errorf(esperavaRecebeu, want, got)
-	}
+	comparaErro(t, got, want)
 }
